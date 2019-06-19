27bb49a654cc2090f4961c08e9cb529eeb5b5d13
Adam Williams <adam.williams@lifeomic.com>
2019-06-19 13:57:05 -0400

    Add config fields for better logging

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,18 @@ import executionHandler from "./executionHandler";
 import invocationValidator from "./invocationValidator";
 
 const invocationConfig: IntegrationInvocationConfig = {
+  instanceConfigFields: {
+    jamfHost: {
+      type: "string",
+    },
+    jamfPassword: {
+      type: "string",
+      mask: true,
+    },
+    jamfUsername: {
+      type: "string",
+    },
+  },
   executionHandler,
   invocationValidator,
 };

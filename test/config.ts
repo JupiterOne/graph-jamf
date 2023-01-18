a93f573d80548be69e87e11b3e68e453792e7d32
eXtremeX <viragsf@gmail.com>
2023-01-18 21:02:24 +0100

    Apply prettier

--- test/config.ts
+++ test/config.ts
@@ -9,8 +9,7 @@ if (process.env.LOAD_ENV) {
 }
 
 export const integrationConfig: IntegrationConfig = {
-  jamfHost:
-    process.env.JAMF_HOST || 'https://jupiteronedev.jamfcloud.com/',
+  jamfHost: process.env.JAMF_HOST || 'https://jupiteronedev.jamfcloud.com/',
   jamfUsername: process.env.JAMF_USERNAME || 'test-username',
   jamfPassword: process.env.JAMF_PASSWORD || 'test-password',
 };
